a5dbc45c83c06d1025672fa76dc778a515ae4e9a
zzne <anders518@outlook.com>
2023-11-29 20:57:59 +0800

    11.29 commit 2

--- src/components/topbar/button.tsx
+++ src/components/topbar/button.tsx
@@ -1,11 +1,11 @@
 import { ComponentProps } from 'react'
 
-type ButtonProps = ComponentProps<'button'>
+type ButtonProps = ComponentProps<'button'> & { variant?: string }
 
 const Button = (props: ButtonProps) => {
   return (
     <button
-      className="hover:border-1 rounded-full  p-2 transition duration-300 hover:border-slate-400 hover:bg-slate-300"
+      className={`hover:border-1 rounded-full  p-2 transition duration-300 hover:border-slate-400 hover:bg-slate-300 ${props.variant}}`}
       {...props}
     />
   )
--- src/components/topbar/index.tsx
+++ src/components/topbar/index.tsx
@@ -11,30 +11,32 @@ import Button from './button'
 
 const Topbar = () => {
   return (
-    <div className="y-2 mx-3 flex items-center justify-between gap-10 py-2 shadow-sm lg:gap-20">
-      <Button>
-        <MenuIcon className="h-6 w-6" />
-      </Button>
-      <Link to="/" className="justify-self-start">
-        <img src="/vite.svg" alt="vite logo" />
-      </Link>
-      <div className="flex flex-row gap-20 justify-self-center">
-        <form className="flex w-64 lg:w-96">
+    <div className="mx-3 flex justify-between gap-10 py-2 lg:gap-20">
+      <div className="flex flex-shrink-0 gap-4">
+        <Button>
+          <MenuIcon className="h-6 w-6" />
+        </Button>
+        <Link to="/">
+          <img src="/vite.svg" alt="vite logo" />
+        </Link>
+      </div>
+      <form className="flex flex-grow justify-center gap-4">
+        <div className="flex max-w-4xl flex-grow">
           <input
             type="text"
             placeholder="Search"
-            className="basis-4/5 justify-center rounded-l-full rounded-r-none border border-r-0  border-slate-300 p-2 shadow-inner"
+            className="basis-4/5 justify-center rounded-l-full rounded-r-none border border-r-0  border-slate-300 p-2 shadow-inner outline-none"
           />
           <button
             type="submit"
-            className="basis-1/5  gap-0 rounded-r-full border border-slate-300 bg-slate-100 hover:bg-slate-300">
+            className="flex-shrink-0 rounded-r-full border px-4 py-2 ">
             <SearchIcon className="h-6 w-6" />
           </button>
-        </form>
-        <Button>
+        </div>
+        <Button variant="bg-slate-300">
           <VoiceIcon className="h-6 w-6" />
         </Button>
-      </div>
+      </form>
       <div className="justify-self-end">
         <Button>
           <UploadIcon className="h-6 w-6" />
